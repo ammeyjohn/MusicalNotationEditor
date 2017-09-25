9637348decedb5873000f8add6790de481ffea27
Patrick <ammeyjohn@gmail.com>
2017-09-25 22:32:10 +0800

    [Feature] Create editor.html, add left/right layout and A4 size paper.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+/*************************************************
+ * 
+ * VexTab http://www.vexflow.com/vextab/tutorial.html
+ * Vocabulary
+ *  notation    谱号
+ *  notes       音符   
+ *  clef        谱号
+ *  key
+ *  tuning      曲调
+ *  rest        休止符
+ *  bar-line    小节线
+ *      =||     Double Bar
+ *      =|:  Repeat Begin 
+ *      =:|  Repeat End 
+ *      =::  Double Repeat 
+ *      =|=  End Bar 
+ *************************************************/
+
+
 'use strict';
 
 import Snap from 'snapsvg';
@@ -5,5 +24,5 @@ import './style.scss';
 
 console.log("index.js.asdfasdf");
 
-var s = Snap('800px', '800px');
+var s = Snap('#svg');
 var bigCircle = s.circle(150, 150, 100);
