3f228fc4896be885ef34feb233b2b22071fa1693
yuanjie <yuanjie@shanghai3h.com>
2017-09-26 15:45:21 +0800

    [Update] Update editor page.

---
+++ editor/editor.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function getDPI() {
+    return document.getElementById("dpi").offsetHeight;
+}
+console.log("DPI =", getDPI());
+
+// A4 = 297*210mm
+function getPixel() {
+    var dpi = getDPI()
+    var INCH_PER_MM = 25.4;
+    var width = 210 / INCH_PER_MM * dpi;
+    var height = 297 / INCH_PER_MM * dpi;
+    $('svg').css({
+        'width': width,
+        'height': height
+    });
+}
+getPixel();
