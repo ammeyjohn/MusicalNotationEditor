9d5791e6309303917f03d548b55a6c21610b44ff
yuanjie <yuanjie@shanghai3h.com>
2017-09-26 18:02:30 +0800

    工作流中增加eslint对js代码进行检查；

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,13 @@ module.exports = {
     },
     module: {
         rules: [{
+            enforce: "pre",
             test: /\.js$/,
-            exclude: /(node_modules)/,
+            exclude: /node_modules/,
+            loader: "eslint-loader",
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
             use: {
                 loader: 'babel-loader',
                 options: {
